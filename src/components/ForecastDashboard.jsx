9dd926de8da8473ddacb3471a432c10b1b10e48c
Edmund <edmundgachanja@gmail.com>
2025-05-20 12:03:29 +0300

    adds month popup

--- src/components/ForecastDashboard.jsx
+++ src/components/ForecastDashboard.jsx
@@ -2,75 +2,124 @@ import Navbar from "./Navbar"
 import Footer from "./Footer"
 import styles from "../styles/forecastDash.module.css"
 
-export default function ForecastDash() {
+import { useState } from "react"
 
-  return (
-    <>
-        <Navbar />
-        <div className={styles.container}>
-            <div className={styles.mainContainer}>
-                
-                <div className={styles.detailsContainer}>
-                    <div className={styles.details}>
-                        <div className={styles.mainCurrent}>
-                            <div className={styles.current}>
-                                <p>Current Income:</p>
-                                <p>Current Surplus:</p>
+export default function ForecastDash() {
+    const [showPopUp, setShowPopUp] = useState(false)
+
+    return (
+        <>
+            <main className={showPopUp ? styles.contentBlur : "" } >
+                <Navbar />
+                <div className={styles.container}>
+                    <div className={styles.mainContainer}>
+                        
+                        <div className={styles.detailsContainer}>
+                            <div className={styles.details}>
+                                <div className={styles.mainCurrent}>
+                                    <div className={styles.current}>
+                                        <p>Current Income:</p>
+                                        <p>Current Surplus:</p>
+                                    </div>
+                                    <div className={styles.current}>
+                                        <p>Current Expense:</p>
+                                        <p>Current Month:</p>
+                                    </div>
+                                </div>
+
+                                <div className={styles.target}>
+                                    <div className={styles.current}>
+                                            <p>Target Income:</p>
+                                            <p>Target Surplus:</p>
+                                        </div>
+                                        <div className={styles.current}>
+                                            <p>Target Expense:</p>
+                                            <p></p>
+                                        </div>
+                                    </div>
                             </div>
-                            <div className={styles.current}>
-                                <p>Current Expense:</p>
-                                <p>Current Month:</p>
+
+                            <div className={styles.surplus}>
+                                <p>Current Surplus / Target Surplus</p>
                             </div>
                         </div>
 
-                        <div className={styles.target}>
-                            <div className={styles.current}>
-                                    <p>Target Income:</p>
-                                    <p>Target Surplus:</p>
+
+                        <div className={styles.monthsContainer}>
+                            <div className={styles.monthSection}>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>January</div>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>February</div>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>March</div>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>April</div>
+                            </div>
+
+                            <div className={styles.monthSection}>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>May</div>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>June</div>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>July</div>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>August</div>
+                            </div>
+
+                            <div className={styles.monthSection}>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>September</div>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>October</div>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>November</div>
+                                <div className={styles.months} onClick={() => setShowPopUp(true)}>Decemeber</div>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <Footer />
+            </main>
+
+            {showPopUp && (
+                <section className={styles.popupOverlay}>
+                    <div className={styles.popup}>
+                        <div className={styles.popupHeader}>
+                            <h2>January</h2>
+                            <button onClick={() => setShowPopUp(false)}
+                                >X
+                            </button>
+                        </div>
+                        <div className={styles.popupDetails}>
+                            <div className={styles.popupSubDetails}>
+                                <div className={styles.popupMiniDetails}>
+                                    <p>logo</p>
+                                    <p>Income</p>
                                 </div>
-                                <div className={styles.current}>
-                                    <p>Target Expense:</p>
-                                    <p></p>
+                                <div>
+                                    <p>1000</p>
                                 </div>
                             </div>
+
+                            <div className={styles.popupSubDetails}>
+                                <div className={styles.popupMiniDetails}>
+                                    <p>logo</p>
+                                    <p>Expense</p>
+                                </div>
+                                <div>
+                                    <p>1000</p>
+                                </div>
+                            </div>
+
+                            <div className={styles.popupSubDetails}>
+                                <div className={styles.popupMiniDetails}>
+                                    <p>logo</p>
+                                    <p>Surplus</p>
+                                </div>
+                                <div>
+                                    <p>1000</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
-                    <div className={styles.surplus}>
-                        <p>Current Surplus / Target Surplus</p>
-                    </div>
-                </div>
-
-
-                <div className={styles.monthsContainer}>
-                    <div className={styles.monthSection}>
-                        <div className={styles.months}>January</div>
-                        <div className={styles.months}>February</div>
-                        <div className={styles.months}>March</div>
-                        <div className={styles.months}>April</div>
-                    </div>
-
-                    <div className={styles.monthSection}>
-                        <div className={styles.months}>May</div>
-                        <div className={styles.months}>June</div>
-                        <div className={styles.months}>July</div>
-                        <div className={styles.months}>August</div>
-                    </div>
-
-                    <div className={styles.monthSection}>
-                        <div className={styles.months}>September</div>
-                        <div className={styles.months}>October</div>
-                        <div className={styles.months}>November</div>
-                        <div className={styles.months}>Decemeber</div>
-                    </div>
-
-                </div>
-
-
-            </div>
-        </div>
-
-        <Footer />
-    </>
+                </section>
+            )}
+        </>
   )
 }
 
