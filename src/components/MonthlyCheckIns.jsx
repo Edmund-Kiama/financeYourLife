2e88f986bdc803cd7af0170a16e24dde3b910056
Edmund <edmundgachanja@gmail.com>
2025-06-02 12:59:25 +0300

    feat: adds content, features with not functionalities yet and styling on some of the empty pages

--- src/components/MonthlyCheckIns.jsx
+++ src/components/MonthlyCheckIns.jsx
@@ -1,13 +1,140 @@
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import styles from "../styles/monthlyCheckIn.module.css"
+
+import { useState } from "react";
+
+import { HiArrowLongRight } from "react-icons/hi2";
+import { SiTicktick } from "react-icons/si";
+import { MdEdit } from "react-icons/md";
+import { RiAddBoxFill } from "react-icons/ri";
 
 export default function MonthlyCheckIns() {
+    const [editPercent1, setEditPercent1] = useState(true)
+    const [editPercent2, setEditPercent2] = useState(true)
+    const [editPercent3, setEditPercent3] = useState(true)
 
     return (
         <>
             <main>
                 <Navbar />
-                <div>3dougnut chart on this i've spent this month</div>
+                <section className={styles.mainContianer}>
+                    <section className={styles.topContainer}>
+                        <h2>June Check-In</h2>
+                        <p>pie chart</p>
+                        <ul>
+                            <li>
+                                <div>
+                                    <p>Total Monthly Income</p>
+                                    <HiArrowLongRight/>
+                                    <p> Kshs 200,000 </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    {/* <p>50%</p> */}
+                                    <section className={styles.input}>
+                                        <input type="number" 
+                                        // onChange={(e) => setTargetIncome(e.target.value)} 
+                                        // value={targetIncome} 
+                                        placeholder="investments: 50%" 
+                                        disabled={editPercent1}
+                                        />
+                                        <button 
+                                        onClick={() => setEditPercent1(x => !x)}>{editPercent1 ?  <MdEdit /> : <SiTicktick />}
+                                        </button>
+                                    </section>
+                                    <HiArrowLongRight/>
+                                    <p> Kshs 200,000 </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                     <section className={styles.input}>
+                                        <input type="number" 
+                                        // onChange={(e) => setTargetIncome(e.target.value)} 
+                                        // value={targetIncome} 
+                                        placeholder="goals: 30%" 
+                                        disabled={editPercent2}
+                                        />
+                                        <button 
+                                        onClick={() => setEditPercent2(x => !x)}>{editPercent2 ?  <MdEdit /> : <SiTicktick />}
+                                        </button>
+                                    </section>
+                                    <HiArrowLongRight/>
+                                    <p> Kshs 200,000 </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                <section className={styles.input}>
+                                        <input type="number" 
+                                        // onChange={(e) => setTargetIncome(e.target.value)} 
+                                        // value={targetIncome} 
+                                        placeholder="fun: 20%" 
+                                        disabled={editPercent3}
+                                        />
+                                        <button 
+                                        onClick={() => setEditPercent3(x => !x)}>{editPercent3 ?  <MdEdit /> : <SiTicktick />}
+                                        </button>
+                                    </section>
+                                    <HiArrowLongRight/>
+                                    <p> Kshs 200,000 </p>
+                                </div>
+                            </li>
+                        </ul>
+                    </section>
+
+                    <section className={styles.midContainer}>
+                        <h2>June Cost So Far</h2>
+                        <div className={styles.inputs}>
+                                <input type="text" placeholder="Description" className={styles.add}
+                                    // value={addIncomeDescri} onChange={(e) => setAddIncomeDescri(e.target.value)}
+                                    />
+                                <input type="number" placeholder="Amount" className={styles.add}
+                                    // value={addIncomeAmount} onChange={(e) => setAddIncomeAmount(e.target.value)}
+                                    />
+                            <RiAddBoxFill size={30} className={styles.add}/>
+
+                            </div>
+                        <ul>
+                            <li>
+                                <p>Description</p>
+                                <p>Amount</p>
+                            </li>
+                            <li>
+                                <p>Description</p>
+                                <p>Amount</p>
+                            </li>
+                            <li>
+                                <p>Description</p>
+                                <p>Amount</p>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <p>Total</p>
+                                <p>Ksh 200,000</p>
+                            </li>
+                        </ul>
+                    </section>
+                </section>
+
+                <section className={styles.bottomContainer}>
+                    <h2>Current Goals</h2>
+                    <div>
+                        <input 
+                            type="text" 
+                            placeholder="Add Goal..."
+                            className={styles.add}/>
+                        <RiAddBoxFill size={30} className={styles.add}/>
+                    </div>
+                    <ol>
+                        <li>250cc Bike</li>
+                        <li>Play Station 5 Slim</li>
+                        <li>Ipad Pro M4</li>
+                    </ol>
+                </section>
 
                 <Footer />
             </main>
--- src/components/Savings&Investments.jsx
+++ src/components/Savings&Investments.jsx
@@ -1,15 +1,99 @@
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import { useState } from "react";
+
+import styles from "../styles/savingsInvestments.module.css"
+
+import { RiAddBoxFill } from "react-icons/ri";
+import { IoIosCloseCircle } from "react-icons/io";
 
 export default function SavingsAndInvestments() {
+    const [showForm, setShowForm] = useState(false)
 
     return (
         <>
             <main>
+                <div className={showForm ? styles.contentBlur : ""}>
                 <Navbar />
-                <div></div>
+                <section className={styles.mainContainer}>
+                    <h2>Savings & Investments</h2>
+                    <div className={styles.heading}>
+                        <h3>Savings & Investments Total: 3</h3>
+                        <RiAddBoxFill  onClick={() => setShowForm(x => !x)} className={styles.button} size={40}/>
+                    </div>
+                    <ul>
+                        <li>
+                            <div className={styles.percentImage}>
+                                image
+                            </div>
+                            <div className={styles.name}>
+                                <p>Britam</p>
+                                <p>15,OOO/month</p>
+                            </div>
+                            <div className={styles.details}>
+                                <p>Current Total: Kshs 20,000</p>
+                                <p>Maturity Target: Kshs 100,000</p>
+                                <p>Maturty Date: 2026</p>
+                            </div>
+                            <div className={styles.chart}>
+                                gauge chart of current total vs maturity target
+                            </div>
+                        </li>
 
-                <Footer />
+                        <li>
+                            <div className={styles.percentImage}>
+                                image
+                            </div>
+                            <div className={styles.name}>
+                                <p>Britam</p>
+                                <p>15,OOO/month</p>
+                            </div>
+                            <div className={styles.details}>
+                                <p>Current Total: Kshs 20,000</p>
+                                <p>Maturity Target: Kshs 100,000</p>
+                                <p>Maturty Date: 2026</p>
+                            </div>
+                            <div className={styles.chart}>
+                                gauge chart of current total vs maturity target
+                            </div>
+                        </li>
+
+                    </ul>
+                </section>
+                {/* <Footer /> */}
+                </div>
+                {showForm && (
+                <section className={styles.popupOverlay}>
+                        <form className={styles.popup}>
+                            <div className={styles.headers}>
+                                <p></p>
+                                <h2>Investment Form</h2>
+                                <IoIosCloseCircle className={styles.iconClose} size={30} onClick={() => setShowForm(x => !x)} />
+                            </div>
+                            <div className={styles.formDetails}>
+                                <div>
+                                    <label>Organization</label>
+                                    <input type="text" placeholder="Orgnization Name" />
+                                </div>
+                                <div>
+                                    <label>Investments per month</label>
+                                    <input type="number" placeholder="Investments/Month" />
+                                </div>
+                                <div>
+                                    <label>Maturity Target</label>
+                                    <input type="number" placeholder="Total Investment Target" />
+                                </div>
+                                <div>
+                                    <label>Maturity Date</label>
+                                    <input type="date" />
+                                </div>
+
+                                <button type="submit">Add</button>
+                            </div>
+                            
+                        </form>
+                </section>
+                )}
             </main>
         </>
   )
